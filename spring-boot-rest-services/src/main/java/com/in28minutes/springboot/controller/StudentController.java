4e4ab06e81342a464863de3aca5f2a6788e61ead
Anil Kumar <aniltpl08@gmail.com>
2018-10-01 18:15:02 +0530

    added swagger doc

--- spring-boot-rest-services/src/main/java/com/in28minutes/springboot/controller/StudentController.java
+++ spring-boot-rest-services/src/main/java/com/in28minutes/springboot/controller/StudentController.java
@@ -1,9 +1,11 @@
 package com.in28minutes.springboot.controller;
 
-import java.net.URI;
-import java.util.List;
-
+import com.in28minutes.springboot.model.Course;
+import com.in28minutes.springboot.model.Student;
+import com.in28minutes.springboot.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,8 +14,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.in28minutes.springboot.model.Course;
-import com.in28minutes.springboot.service.StudentService;
+import java.net.URI;
+import java.util.List;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 public class StudentController {
@@ -21,13 +26,32 @@ public class StudentController {
 	@Autowired
 	private StudentService studentService;
 
+
+	@GetMapping("/students/{studentId}")
+	public Resource<Student> retrieveStudentById(@PathVariable String studentId){
+		Student student = studentService.retrieveStudent( studentId );
+
+		Resource<Student> resource = new Resource<Student>(student);
+
+		ControllerLinkBuilder linkTo = linkTo( methodOn( this.getClass() )
+				.retrieveStudents() );
+		resource.add( linkTo.withRel("all-students") );
+		return resource;
+	}
+
+
 	@GetMapping("/students/{studentId}/courses")
-	public List<Course> retrieveCoursesForStudent(@PathVariable String studentId) {
+	public List<Course> retrieveCoursesForStudent(
+			@PathVariable String studentId) {
 		return studentService.retrieveCourses(studentId);
+	}
+	@GetMapping("/students")
+	public List<Student> retrieveStudents() {
+		return studentService.retrieveAllStudents();
 	}
 
 	@PostMapping("/students/{studentId}/courses")
-	public ResponseEntity<Void> registerStudentForCourse(
+	public ResponseEntity<Resource> registerStudentForCourse(
 			@PathVariable String studentId, @RequestBody Course newCourse) {
 
 		Course course = studentService.addCourse(studentId, newCourse);
@@ -35,10 +59,16 @@ public class StudentController {
 		if (course == null)
 			return ResponseEntity.noContent().build();
 
+		Resource<Course> resource = new Resource<>(course);
+
+		final ControllerLinkBuilder linkBuilder = linkTo(methodOn(this.getClass())
+				.retrieveDetailsForCourse(studentId, course.getId()));
+		resource.add(linkBuilder.withSelfRel());
+
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path(
 				"/{id}").buildAndExpand(course.getId()).toUri();
 
-		return ResponseEntity.created(location).build();
+		return ResponseEntity.created(location).body(resource);
 	}
 
 	@GetMapping("/students/{studentId}/courses/{courseId}")
